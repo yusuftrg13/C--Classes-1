bf68a3d88d8076747de45e2e556f074e2ba63d12
Ytr <132002974+yusuftrg13@users.noreply.github.com>
2023-12-29 19:25:07 +0300

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_oop_calisma
+{
+    internal class Car
+    {
+        // Class'ın özellikleri tanımladık.
+        public string marka;
+        public string model;
+        public int engine;
+        public int km;
+
+
+        
+
+
+        public Car(string amarka,string amodel,int aengine,int akm) //Yapıcı metod
+        {
+            marka=amarka;
+            model=amodel;
+            engine = aengine;
+            km=akm;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// C#'DA OOP KONUSUNA GİRİS YAPACAGIM
+//Proje ekle kısmından bir yeni class eklemesi yaparız.
+
+using Csharp_oop_calisma;
+
+Car car1 = new Car("Ford","Ranger",4,15000); // car1 Car class'ından türetilen bir aractır.
+
+
+Car car2 = new Car();
+car2.marka = "Fiat";
+car2.model = "Egea";  //Farklı nesne tanımlama yöntemi
+car2.engine = 4;
+car2.km = 90000;
+
+Console.WriteLine(car1.marka + " "+ car1.model); //Class elemanlarına göre cagrım yapabiliriz.
+Console.WriteLine(car1.marka); //Yapıcı metodumuzun nesnesini cagırdık.
+
+
+
+
+
+Console.ReadLine();
